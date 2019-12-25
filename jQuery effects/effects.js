6584369b394aed5f28d415110b4927413db2161e
jugaltoppo <58083190+jugaltoppo@users.noreply.github.com>
2019-12-25 19:01:51 +0530

    Update effects.js

--- jQuery effects/effects.js
+++ jQuery effects/effects.js
@@ -1,1 +1,40 @@
-alert("connected");
+//to fade an element out
+$("button#out").on("click",function(){
+    $("div.hide").fadeOut(3000,function(){
+        $(this).remove(); // '.remove' removes the elements from the DOM, omitting it will only set the 'display' to 'none'
+    });
+});
+
+//to fade an element in
+$("button#in").on("click",function(){
+    $("div.show").fadeIn(3000);
+});
+
+//do both at once 
+$("button#both").on("click",function(){
+    $("div.hide").fadeOut(3000,function(){
+        $("div.show").fadeIn(3000);
+    });
+});
+
+//fade toggle
+$("button#toggle").on("click",function(){
+    $("div.fadeToggle").fadeToggle(3000);
+});
+
+//slide up
+$("#btnslideup").on("click",function(){
+    $(".slide").slideUp(3000);
+});
+
+//slide down
+$("#btnslidedown").on("click",function(){
+    $(".slidedwn").slideDown(3000);
+});
+
+//toggle slide
+$("button#toggleslide").on("click",function(){
+    $("div.slidetoggle").slideToggle(3000,function(){
+        console.log("slide toggle is done");
+    });
+});
